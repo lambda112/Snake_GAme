4324db8e6c142bd74212b4c92578bd20b4e28e6f
Kamran Azim <Kamazim121212@gmail.com>
2024-03-08 15:07:20 +0000

    Switch to oop approach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle as t
 from time import sleep
+from snake import Snake
 
 # Setup Screen
 screen = t.Screen()
@@ -8,32 +9,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
 
-# Create Snake Body
-starting_positions = [(0,0), (-20,0), (-40, 0)]
-segments = [] 
-
-for position in starting_positions:
-    new_segment = t.Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
-
-# Animate Snake
+# Move Snake
+snake = Snake()
 game_is_on = True
-
 while game_is_on:
     screen.update()
     sleep(0.1)
-
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-
-    segments[0].forward(20)
-
+    snake.move()
 
 # Close on Click
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,25 @@
+import turtle as t
+from time import sleep
+
+
+class Snake:
+    def __init__(self) -> None:
+        self.starting_positions = [(0,0), (-20,0), (-40, 0)]
+        self.segments = [] 
+
+        # Create Snake
+        for position in self.starting_positions:
+            new_segment = t.Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        # Animate Snake
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.segments[0].forward(20)
